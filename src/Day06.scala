5a46a2d4efba34b54920fdeda50c61b9baddebe2
Sztorm <marcinwicher99@gmail.com>
2025-12-06 11:19:28 +0100

    Added Day06 solution.

--- src/Day06.scala
+++ src/Day06.scala
@@ -1,4 +1,64 @@
-object Day06:
-  def part1(input: List[String]) = 0
+private enum Operation:
+  case Sum, Product
 
-  def part2(input: List[String]) = 0
+private case class Homework(numbers: List[Long], operation: Operation):
+  def solve = operation match
+    case Operation.Sum => numbers.sum
+    case Operation.Product => numbers.product
+
+object Day06:
+  private def parseOperations(input: String) = input
+    .split(' ')
+    .filterNot(_.isBlank)
+    .map {
+      case "+" => Operation.Sum
+      case _ => Operation.Product
+    }
+    .toList
+
+  private def parseHomeworks(input: List[String]) = input
+    .splitAt(input.length - 1)
+    .|>((numberRows, operationRow) =>
+      val numberColumns = numberRows
+        .map(row => row
+          .split(' ')
+          .filterNot(_.isBlank)
+          .map(_.toLong)
+          .toList
+        )
+        .transpose
+      val operations = parseOperations(operationRow.head)
+
+      numberColumns
+        .zip(operations)
+        .map((column, operation) => Homework(column, operation))
+    )
+
+  private def parseCephalopodHomeworks(input: List[String]) = input
+    .splitAt(input.length - 1)
+    .|>((numberRows, operationRow) =>
+      val maxLength = numberRows.maxBy(_.length).length
+      val paddedNumbers = numberRows.map(_.padTo(maxLength, ' '))
+      val numberColumns = paddedNumbers
+        .transpose
+        .map(_.filterNot(_.isWhitespace))
+        .split(_.isEmpty)
+        .map(column => column
+          .map(_.mkString.toLong)
+        )
+      val operations = parseOperations(operationRow.head)
+
+      numberColumns
+        .zip(operations)
+        .map((column, operation) => Homework(column, operation))
+    )
+
+  def part1(input: List[String]) = input
+    .|>(parseHomeworks)
+    .map(_.solve)
+    .sum
+
+  def part2(input: List[String]) = input
+    .|>(parseCephalopodHomeworks)
+    .map(_.solve)
+    .sum
--- src/Extensions.scala
+++ src/Extensions.scala
@@ -3,3 +3,12 @@ extension [A](value: A)
 
 extension (value: Boolean)
   def toInt = if value then 1 else 0
+
+extension [A](list: List[A])
+  def split(p: A => Boolean): List[List[A]] = list
+    .foldRight(List(List.empty)) { (item, acc) =>
+      if p(item) then List.empty :: acc
+      else acc match
+        case Nil => List(item) :: acc
+        case head :: tail => (item :: head) :: tail
+    }
